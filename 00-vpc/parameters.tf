10e7fff83446dacfcfebafc3937e04be3aa099bf
dharsha27 <dharshavardhan27@gmail.com>
2026-09-20 16:29:56 +0530

    changes done

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter"  "vpc_id"{
-   name = "/${var.project}-${var.environment}/vpc_id"
+   name = "/${var.project}/${var.environment}/vpc_id"
    type = "String"
    value = module.vpc.vpc_id
    overwrite=true
@@ -7,7 +7,7 @@ resource "aws_ssm_parameter"  "vpc_id"{
 }
 
 resource "aws_ssm_parameter"  "public_subnet_ids"{
-   name = "/${var.project}-${var.environment}/public_subnet_ids"
+   name = "/${var.project}/${var.environment}/public_subnet_ids"
    type = "String"
    value = join(",",module.vpc.public_subnet_ids)
    overwrite=true
@@ -15,7 +15,7 @@ resource "aws_ssm_parameter"  "public_subnet_ids"{
 }
 
 resource "aws_ssm_parameter"  "private_subnet_ids"{
-   name = "/${var.project}-${var.environment}/private_subnet_ids"
+   name = "/${var.project}/${var.environment}/private_subnet_ids"
    type = "String"
    value = join(",",module.vpc.private_subnet_ids)
    overwrite=true
@@ -23,7 +23,7 @@ resource "aws_ssm_parameter"  "private_subnet_ids"{
 }
 
 resource "aws_ssm_parameter"  "database_subnet_ids"{
-   name = "/${var.project}-${var.environment}/database_subnet_ids"
+   name = "/${var.project}/${var.environment}/database_subnet_ids"
    type = "String"
    value = join(",",module.vpc.database_subnet_ids)
    overwrite=true

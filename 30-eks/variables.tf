1ff972a5c2aa49552b3a0e125535441ff909b84d
dharsha27 <dharshavardhan27@gmail.com>
2026-09-22 15:36:22 +0530

    changes done

--- 30-eks/variables.tf
+++ 30-eks/variables.tf
@@ -1,27 +1,27 @@
 variable "project" {
-    default = "roboshop"
+  default = "roboshop"
 }
 
 variable "environment" {
-    default = "dev"
+  default = "dev"
 }
 
 variable "eks_version" {
-    default = "1.36"
+  default = "1.36"
 }
 
 variable "enable_blue" {
-    default = true
+  default = true
 }
 
 variable "blue_version" {
-    default = "1.36"
+  default = "1.36"
 }
 
 variable "enable_green" {
-    default = false
+  default = false
 }
 
 variable "green_version" {
-    default = "1.35"
+  default = "1.35"
 }
